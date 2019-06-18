0c8a36ec9061680db27cf4b590f9ef7726f529d5
Bob <test@test.com>
2019-06-18 19:47:54 +1000

    wip

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # traefikrp
+
+Needs docker.io, docker-compose, curl, uuid
+
+Add to /etc/hosts:
+127.0.0.1	auth.localnet
--- run_rp.sh
+++ run_rp.sh
@@ -16,7 +16,7 @@ echo " \e[32mdone\e[39m"
 
 # Start Keyclock
 echo "*** Starting Keycloak ***"
-sudo rm -rf ./data/*
+sudo ./clean-db.sh
 sed 's/{{ LOCAL_IP }}/'"$LOCAL_IP"'/g' realm.json.template > keycloak/realm.json
 sudo docker-compose up -d keycloak
 
@@ -29,14 +29,14 @@ echo " \e[32mdone\e[39m"
 
 # Create initial user
 echo -n "Creating initial app user ..."
-sudo docker exec -it traefikrp_keycloak_1 /opt/jboss/keycloak/bin/kcadm.sh create users -r rp -s username=testuser -s enabled=true --server http://$LOCAL_IP:8080/auth --realm master --user admin --password admin > /dev/null
-sudo docker exec -it traefikrp_keycloak_1 /opt/jboss/keycloak/bin/kcadm.sh set-password -r rp --username testuser --new-password NEWPASSWORD --temporary --server http://$LOCAL_IP:8080/auth --realm master --user admin --password admin > /dev/null
+sudo docker exec -it traefikrp_keycloak_1 /opt/jboss/keycloak/bin/kcadm.sh create users -r rp -s username=testuser -s enabled=true --server http://127.0.0.1:8080/auth --realm master --user admin --password admin > /dev/null
+sudo docker exec -it traefikrp_keycloak_1 /opt/jboss/keycloak/bin/kcadm.sh set-password -r rp --username testuser --new-password NEWPASSWORD --temporary --server http://127.0.0.1:8080/auth --realm master --user admin --password admin > /dev/null
 echo " \e[32mdone\e[39m"
 
 # Generate a new secret and update Keycloak client
 echo -n "Generating new client secret ..."
 SECRET=$(uuid)
-sudo docker exec -it traefikrp_keycloak_1 /opt/jboss/keycloak/bin/kcadm.sh update clients/ff88533c-bb46-4f0d-a3ef-de47e1c4ad4d -r rp -s secret=$SECRET --server http://$LOCAL_IP:8080/auth --realm master --user admin --password admin > /dev/null
+sudo docker exec -it traefikrp_keycloak_1 /opt/jboss/keycloak/bin/kcadm.sh update clients/ff88533c-bb46-4f0d-a3ef-de47e1c4ad4d -r rp -s secret=$SECRET --server http://127.0.0.1:8080/auth --realm master --user admin --password admin > /dev/null
 echo " \e[32mdone\e[39m"
 
 # Update GK config and build
